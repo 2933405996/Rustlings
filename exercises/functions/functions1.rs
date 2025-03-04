c207fd91c3fb65fad02d9f9fb6d3bd162626cc42
15772782145 <2933405996@qq.com>
2025-03-04 13:37:32 +0800

    update

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -5,6 +5,11 @@
 
 // I AM NOT DONE
 
+fn call_me() {
+    println!("I called call_me!");
+}
+/******************** 4ee1a76e-2e13-416d-b572-5a112590e40c ********************/
+
 fn main() {
     call_me();
 }
